7c64078a2b6b45712111c13a449f0301c21c87df
lane <lane@joeandlane.com>
2020-01-10 14:22:45 -0500

    Still fixing build

--- src/resp/command/string/mset-command.ts
+++ src/resp/command/string/mset-command.ts
@@ -11,17 +11,11 @@ import { IRespCommand } from '../resp-command';
  *
  * MSET key value [key value ...]
  *
-<<<<<<< HEAD
- * Sets the given keys to their respective values. MSET replaces existing values with new values, just as regular SET. See MSETNX if you don't want to overwrite existing values.
- *
- * MSET is atomic, so all given keys are set at once. It is not possible for clients to see that some of the keys were updated while others are unchanged.
-=======
  * Sets the given keys to their respective values. MSET replaces existing values with new
  * values, just as regular SET. See MSETNX if you don't want to overwrite existing values.
  *
  * MSET is atomic, so all given keys are set at once. It is not possible for clients to see
  * that some of the keys were updated while others are unchanged.
->>>>>>> dev
  *
  * **Return value**<br>
  * Simple string reply: always OK since MSET can't fail.
