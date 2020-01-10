aae4edda37a9c1d22ceea4b8f5e2661f89b9ee01
lane <lane@joeandlane.com>
2020-01-10 13:52:09 -0500

    Corrected lint errors

--- src/resp/command/string/mset-command.ts
+++ src/resp/command/string/mset-command.ts
@@ -1,8 +1,8 @@
 import { MaxParams, MinParams, Name } from '../../../decorators';
 import { Logger } from '../../../logger';
 import { IRequest } from '../../../server/request';
-import { Database } from '../../data/database';
 import { DataType } from '../../data/data-type';
+import { Database } from '../../data/database';
 import { DatabaseValue } from '../../data/database-value';
 import { RedisToken } from '../../protocol/redis-token';
 import { IRespCommand } from '../resp-command';
@@ -11,9 +11,11 @@ import { IRespCommand } from '../resp-command';
  *
  * MSET key value [key value ...]
  *
- * Sets the given keys to their respective values. MSET replaces existing values with new values, just as regular SET. See MSETNX if you don't want to overwrite existing values.
+ * Sets the given keys to their respective values. MSET replaces existing values with new
+ * values, just as regular SET. See MSETNX if you don't want to overwrite existing values.
  *
- * MSET is atomic, so all given keys are set at once. It is not possible for clients to see that some of the keys were updated while others are unchanged.
+ * MSET is atomic, so all given keys are set at once. It is not possible for clients to see
+ * that some of the keys were updated while others are unchanged.
  *
  * **Return value**<br>
  * Simple string reply: always OK since MSET can't fail.
@@ -29,9 +31,9 @@ export class MsetCommand implements IRespCommand {
     if (request.getParams().length % 2 !== 0) {
       return RedisToken.error('ERR wrong number of arguments for \'mset\' command');
     }
-    for (let index = 0; index<request.getParams().length; index+=2) {
+    for (let index = 0; index < request.getParams().length; index += 2) {
       const key: string = request.getParam(index);
-      const value: string = request.getParam(index+1);
+      const value: string = request.getParam(index + 1);
       this.logger.debug(`Setting key ${key} to "${value}"`);
       db.put(key, new DatabaseValue(DataType.STRING, value));
     }
